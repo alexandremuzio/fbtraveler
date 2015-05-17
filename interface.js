4235f00e0f404e371f674dbbe84b57d5c984000d
unknown <ax.muzio@gmail.com>
2015-05-17 02:30:47 -0300

    Integrating Google Maps API files

--- interface.js
+++ interface.js
@@ -1,4 +1,7 @@
-function create_element(photo) {
+var elementsLoaded = 0;
+var numberOfInitialResults = 9;
+
+function createElement(photo) {
 	console.log(photo);
 	return $.parseHTML('<div class="col-md-4 portfolio-item"> \
 		<a href="' + photo.link + '" target="blank"> <img class="img-responsive" + src="' + photo.url + '" alt=""> </a>\
@@ -7,13 +10,13 @@ function create_element(photo) {
 		</div>');
 }
 
-function create_row(id, photoList) {
+function createRow(id, photoList) {
 	var father_div = $("#search_results");
 
 	var $div = $("<div>", {id: "row" + id});
 
 	for (var i = 0; i < 3; i++) {
-		var element = create_element(photoList[i]);
+		var element = createElement(photoList[i]);
 		father_div.append(element);
 	}
 	father_div.append($div);
@@ -27,11 +30,37 @@ function searchByKeyword() {
 	var listFb = searchForFriendsPhotosMock(keyword);
 	//var listFlickr = GetByKeyWordFlickr(keyword);
 
+	//show initial results
 	console.log(listFb.length);
-	for (var i = 0; i < listFb.length; i = i + 4) {
+	for (var i = 0; i < listFb.length && i < numberOfInitialResults; i = i + 4) {
+		elementsLoaded++;
 		var tempList = listFb.slice(i, i+3);
-		create_row(i, tempList);
+		createRow(i, tempList);
 	}
 	console.log(listFb);
-	create_row(1);
+	createRow(1);
 }
+
+
+function showMap(list) {
+	points = list;
+	//...
+}
+
+//after having created initial results
+function loadNewRow() {
+	elementsLoaded = 0;
+	for (var i = elementsLoaded; i < listFb.length; i = i + 4) {
+	elementsLoaded++;
+	var tempList = listFb.slice(i, i+3);
+	createRow(i, tempList);
+	}
+}
+
+
+$(window).scroll(function() {
+    if($(window).scrollTop() == $(document).height() - $(window).height()) {
+    	//loadNewRow();
+    	console.log("Reached bottom");
+    }
+});
